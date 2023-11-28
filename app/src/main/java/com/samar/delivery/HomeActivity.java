d1b37d2673b0db3d2aadd12793d37da6f86a8f72
Toumi Mohamed Koussay <toumikoussay@hotmail.fr>
2023-11-28 14:52:53 +0100

    Fix Bug : Closing the app correctly on back pressed

--- app/src/main/java/com/samar/delivery/HomeActivity.java
+++ app/src/main/java/com/samar/delivery/HomeActivity.java
@@ -506,6 +506,11 @@ public class HomeActivity extends AppCompatActivity {
                     .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+
+                            Intent intent = new Intent(Intent.ACTION_MAIN);
+                            intent.addCategory(Intent.CATEGORY_HOME);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
                             finish();
                         }
                     })
